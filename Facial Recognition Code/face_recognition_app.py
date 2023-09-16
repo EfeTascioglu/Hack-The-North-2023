be8d37ae06de5a327d8da69fbe4f9e57095dc03a
Efe Tascioglu <efetoronto@gmail.com>
2023-09-16 11:20:14 -0400

    Multi-Face Recognition with locations (no display) finished

--- Facial Recognition Code/face_recognition_app.py
+++ Facial Recognition Code/face_recognition_app.py
@@ -21,19 +21,28 @@ class SimpleFaceRecognizer:
     def recognize(self, unknown_image_path):
         unknown_image = face_recognition.load_image_file(unknown_image_path)
         unknown_face_encodings = face_recognition.face_encodings(unknown_image)
+        unknown_face_locations = face_recognition.face_locations(unknown_image)
         
-        for unknown_face_encoding in unknown_face_encodings:
-            # Compare the face to known faces
-            results = face_recognition.compare_faces(self.encodings, unknown_face_encoding)
+        recognized_faces = []
+
+        for unknown_face_encoding, face_location in zip(unknown_face_encodings, unknown_face_locations):
+            matches = face_recognition.compare_faces(self.encodings, unknown_face_encoding)
             name = "Unknown"
-            for i in range(len(results)):
-                if results[i]:
-                    name = self.names[i]
-                    break
-            return name
+            if True in matches:
+                match_index = matches.index(True)
+                name = self.names[match_index]
+            recognized_faces.append({
+                "name": name,
+                "location": face_location  # This will be in the format (top, right, bottom, left)
+            })
+
+        return recognized_faces
 
 # Example usage:
 recognizer = SimpleFaceRecognizer()
 recognizer.add_face(get_absolute_path("images/efe_1.jpg"), "Efe Tascioglu")
 recognizer.add_face(get_absolute_path("images/tyler_1.jpg"), "Tyler Tian")
-print(recognizer.recognize(get_absolute_path("images/tyler_2.jpg")))
+
+recognized_faces = recognizer.recognize(get_absolute_path("images/tyler_2.jpg"))
+for face in recognized_faces:
+    print(f"Name: {face['name']}, Location: {face['location']}")
