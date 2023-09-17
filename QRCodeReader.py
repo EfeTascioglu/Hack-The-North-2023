9676598402e7fa05913744bc41084473aec7be43
andygongexe <andygongexe@gmail.com>
2023-09-16 21:30:36 -0400

    made QRCodeReader a function

--- QRCodeReader.py
+++ QRCodeReader.py
@@ -1,6 +1,19 @@
 import cv2
-img = cv2.imread("Image.jpg")
-detect = cv2.QRCodeDetector()
-val, pts, st_code = detect.detectAndDecode(img)
 
-print(val, pts, st_code)
+def QR_read(image = "image.png"):
+    """
+    This function reads the QR code from the image and returns the decoded value, the points of the QR code and the status code.
+    
+    :param image: The image from which the QR code is to be read.
+    :return: The decoded value, the points of the QR code and the status code.
+    """
+        
+    img = cv2.imread(image)
+    detect = cv2.QRCodeDetector()
+    val, pts, st_code = detect.detectAndDecode(img)
+
+    # print(val, pts, st_code)
+    return val, pts, st_code
+
+if __name__ == "__main__":
+    QR_read()
