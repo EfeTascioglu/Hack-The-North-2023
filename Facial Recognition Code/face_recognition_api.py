39eb4686c25b72d9210b72c08dffe80cd9c0f81f
Efe Tascioglu <efetoronto@gmail.com>
2023-09-16 21:26:17 -0400

    Added Images for Training

--- Facial Recognition Code/face_recognition_api.py
+++ Facial Recognition Code/face_recognition_api.py
@@ -97,5 +97,10 @@ class SimpleFaceRecognizer:
 if __name__ == "__main__":
     # Example usage:
     recognizer = SimpleFaceRecognizer()
+    # Set up the database in RAM
+    recognizer.add_face(get_absolute_path("images/efe_1.jpg"), "Efe Tascioglu")
+    recognizer.add_face(get_absolute_path("images/efe_2.jpg"), "Efe Tascioglu")
+    recognizer.add_face(get_absolute_path("images/efe_3.jpg"), "Efe Tascioglu")
+    recognizer.add_face(get_absolute_path("images/tyler_1.jpg"), "Tyler Tian")
     app.run(host='0.0.0.0')
     
