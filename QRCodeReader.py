152724924b90ad6e35ed6cbc43c3e4a279e1c7c2
andygongexe <andygongexe@gmail.com>
2023-09-16 23:01:42 -0400

    accepted inevitability of many qr codes

--- QRCodeReader.py
+++ QRCodeReader.py
@@ -3,6 +3,7 @@ import cv2
 def QR_read(image = "image.png"):
     """
     This function reads the QR code from the image and returns the decoded value, the points of the QR code and the status code.
+    We assume there will only be one QR code in the image.
     
     :param image: The image from which the QR code is to be read.
     :return: The decoded value, the points of the QR code and the status code.
